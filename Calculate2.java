814fd741b305f69bf8106607d1e74d87d40f441a
V061005 <vaishnavilakkundi@gmail.com>
2025-04-28 14:14:23 +0530

    save the file in main

---
+++ Calculate2.java
@@ -0,0 +1,11 @@
+public class Calculate {
+    public static void main(String[] args) {
+        a=5;
+        b=10;
+        System.out.println("Addition"+(a+b));
+        System.out.println("Subtraction"+(a-b));
+        System.out.println("Multiplication"+(a*b));
+        System.out.println("Division"+(a/b));
+
+    }
+}
